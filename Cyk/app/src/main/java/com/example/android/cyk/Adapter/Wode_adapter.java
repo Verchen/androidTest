2cf5cf94f1a5730a404f74e1855abc9432734e82
qiao <iosqiao@qq.com>
2017-07-05 15:37:36 +0800

    我的页面搭建

---
+++ Cyk/app/src/main/java/com/example/android/cyk/Adapter/Wode_adapter.java
@@ -0,0 +1,119 @@
+package com.example.android.cyk.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.android.cyk.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by qiao on 2017/7/5.
+ */
+
+public class Wode_adapter extends BaseAdapter {
+
+    Context mContext;
+    ArrayList<String> dataSource;
+
+    public Wode_adapter (Context context, ArrayList<String> datas){
+        this.mContext=context;
+        this.dataSource=datas;
+    }
+
+    @Override
+    public int getCount() {
+        return 6;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return 1;
+        }
+        if (position == 5) {
+            return 3;
+        }else {
+            return 2;
+        }
+
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return dataSource.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        int type = getItemViewType(i);
+
+        if (type == 1){
+            view = inflater.inflate(R.layout.wode_touxiang_layout, viewGroup, false);
+            return view;
+        }
+        if (type == 2){
+            view = inflater.inflate(R.layout.wo_menu_item_layout, viewGroup, false);
+            switch (i){
+                case 1:
+                {
+                    ImageView imageView = view.findViewById(R.id.id_wo_menu_img);
+                    imageView.setImageResource(R.mipmap.ic_launcher);
+                    TextView textView = view.findViewById(R.id.id_wo_menu_tv);
+                    textView.setText("我的信息");
+                    break;
+                }
+
+                case 2:
+                {
+                    ImageView imageView = view.findViewById(R.id.id_wo_menu_img);
+                    imageView.setImageResource(R.mipmap.ic_launcher);
+                    TextView textView = view.findViewById(R.id.id_wo_menu_tv);
+                    textView.setText("账单");
+                    break;
+                }
+                case 3:
+                {
+                    ImageView imageView = view.findViewById(R.id.id_wo_menu_img);
+                    imageView.setImageResource(R.mipmap.ic_launcher);
+                    TextView textView = view.findViewById(R.id.id_wo_menu_tv);
+                    textView.setText("借款记录");
+                    break;
+                }
+                case 4:
+                {
+                    ImageView imageView = view.findViewById(R.id.id_wo_menu_img);
+                    imageView.setImageResource(R.mipmap.ic_launcher);
+                    TextView textView = view.findViewById(R.id.id_wo_menu_tv);
+                    textView.setText("更改密码");
+                    break;
+                }
+                case 5:
+                {
+                    ImageView imageView = view.findViewById(R.id.id_wo_menu_img);
+                    imageView.setImageResource(R.mipmap.ic_launcher);
+                    TextView textView = view.findViewById(R.id.id_wo_menu_tv);
+                    textView.setText("退出登录");
+                    break;
+                }
+            }
+            return view;
+        }else {
+            view = inflater.inflate(R.layout.wode_tuichu_item_layout, viewGroup, false);
+            return view;
+        }
+    }
+}
--- Cyk/app/src/main/java/com/example/android/cyk/WodeFragment.java
+++ Cyk/app/src/main/java/com/example/android/cyk/WodeFragment.java
@@ -1,11 +1,17 @@
 package com.example.android.cyk;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.android.cyk.Adapter.Wode_adapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by qiao on 2017/7/3.
@@ -13,9 +19,34 @@ import android.view.ViewGroup;
 
 public class WodeFragment extends Fragment {
 
+    Context mContext;
+    ListView listView;
+    Wode_adapter wode_adapter;
+    ArrayList<String> listString;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext=getContext();
+        listString = new ArrayList<String>();
+        for (int i = 0; i < 10; i++)
+        {
+            listString.add(Integer.toString(i));
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.wode_layout, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        listView = getView().findViewById(R.id.id_wode_list_view);
+        wode_adapter = new Wode_adapter(mContext, listString);
+        listView.setAdapter(wode_adapter);
     }
 }
